f73b69458cf7d555085e8b2ec44df2f0be4d05db
Jason rader <Jasonmrader@outlook.com>
2022-06-23 14:20:35 -0400

    Add project files.

---
+++ TimeLog/Activity.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace TimeLog
+{
+    public class Activity
+    {
+        public Activity() { }
+        [XmlElement(ElementName = "Name", Order = 1)]
+        public string Name { get; set; }
+
+        [XmlArray(ElementName = "Events", Order = 2)]
+        public List<Event> Events = new List<Event>();
+        
+        public void StartCurrentActivity(DateTime start)
+        {
+            Event e = new Event(start);
+            Events.Add(e);
+
+        }
+        public void EndCurrentActivity(DateTime start)
+        {
+            foreach(Event e in Events.Where(x => x.StartTime == start))
+            {
+                e.EndTime = DateTime.Now;
+            }
+        }
+        public void AddPastEvent(string name, DateTime start, DateTime end)
+        {
+            name = this.Name;
+            Events.Add(new Event(name, start, end));
+        }
+
+        public static List<Event> GetEvents (DateTime fromTime, DateTime toTime)
+        {
+            List<Event> events = new();
+            List <Activity> activityList = new List<Activity>(GetActivityList());
+            foreach (Activity activity in activityList)
+            {
+                foreach (Event e in activity.Events)
+                {
+                    if (e.StartTime > fromTime && e.StartTime < toTime)
+                    {
+                        events.Add(e);
+                    }
+                }
+            }
+
+            return events;
+
+        }
+        
+        public static List<Activity> GetActivityList()
+        {
+            List<Activity> list = new List<Activity>();
+            var filePath = @"C:\Users\Jason\source\repos\TimeLog\TimeLog\XmlFiles\Activities.xml";
+            var xs = new XmlSerializer(typeof(List<Activity>));
+
+            using (Stream s = File.OpenRead(filePath))
+                list = (List<Activity>)xs.Deserialize(s);
+            return list;
+
+
+        }
+
+        public static void SaveActivityList(List<Activity> list)
+        {
+            var filePath = @"C:\Users\Jason\source\repos\TimeLog\TimeLog\XmlFiles\Activities.xml";
+
+            var xs = new XmlSerializer(typeof(List<Activity>));
+            using (Stream s = File.Create(filePath))
+                xs.Serialize(s, list);
+        }
+    }
+    
+
+}
---
+++ TimeLog/Event.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace TimeLog
+{
+    public class Event
+    {
+        public Event() { }
+        public Event(DateTime startTime) 
+        {
+            this.StartTime = startTime;
+        }
+        public Event (DateTime startTime, DateTime endTime)
+        {
+            this.EndTime = endTime;
+            this.StartTime = startTime;
+        }
+        public Event(string name, DateTime startTime, DateTime endTime)
+        {
+            this.Name = name;
+            this.EndTime = endTime;
+            this.StartTime = startTime;
+        }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        [XmlAttribute]public string Name { get; set; }
+        public TimeSpan Duration
+        {
+            get 
+            {
+                TimeSpan duration = EndTime - StartTime;
+                return duration; 
+            }
+            
+        }
+        
+    }
+}
+
